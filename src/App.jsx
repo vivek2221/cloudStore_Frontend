efba933f360ee70b2e0c9f55b93c5da454e661d0
vivek2221 <vshekhawat136@gmail.com>
2026-06-28 09:54:01 +0530

    adding obtimising visualizer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
+const Dashboard = lazy(()=>'./pages/Dashboard')
 import Upgrade from './pages/Upgrade';
 
 function App() {
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    visualizer({
+      open: true, // Automatically opens the browser stats page after building
+      filename: 'bundle-analysis.html', // Name of the file it will generate
+      gzipSize: true, // Shows you how big the file will be when compressed
+    })
+  ],
 })
