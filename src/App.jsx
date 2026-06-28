493bc29dc6bf6e536466ef8aa698c16d75129347
vivek2221 <vshekhawat136@gmail.com>
2026-06-28 09:58:43 +0530

    removing the error due to visualiser

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-const Dashboard = lazy(()=>'./pages/Dashboard')
+let Dashboard = lazy(()=>'./pages/Dashboard')
 import Upgrade from './pages/Upgrade';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter fallback={<div>loading...</div>}>
+    <Suspense>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/upgrade" element={<Upgrade />} />
       </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
